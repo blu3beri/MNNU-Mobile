578bc6a262aea8823bfabfa8daffaa3fa7e20bf5
blu3beri <dev@blueberi.nl>
2020-11-25 11:21:44 +0100

    huh no more React Native?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { Plugins, StatusBarStyle } from "@capacitor/core";
+
+const { StatusBar } = Plugins;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
+  constructor(private platform: Platform, private splashScreen: SplashScreen) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
       this.splashScreen.hide();
+      StatusBar.setBackgroundColor({ color: StatusBarStyle.Light });
     });
   }
+
+  setStatusBar() {}
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { Plugins, StatusBarStyle } from "@capacitor/core";
+
+const { StatusBar } = Plugins;
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  selector: "app-tab2",
+  templateUrl: "tab2.page.html",
+  styleUrls: ["tab2.page.scss"],
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  isStatusBarLight = true;
 
-  constructor() {}
+  changeStatusBar() {
+    StatusBar.setStyle({
+      style: StatusBarStyle.Light,
+    });
+    this.isStatusBarLight = !this.isStatusBarLight;
 
+    // Display content under transparent status bar (Android only)
+    StatusBar.setOverlaysWebView({
+      overlay: true,
+    });
+  }
+
+  hideStatusBar() {
+    StatusBar.hide();
+  }
+
+  showStatusBar() {
+    StatusBar.show();
+  }
+
+  ngOnInit() {
+    this.changeStatusBar();
+  }
 }
